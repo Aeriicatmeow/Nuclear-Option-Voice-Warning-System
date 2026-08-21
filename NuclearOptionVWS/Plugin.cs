03855b21d72e6c0b81bf37b19f8ea740f97aac36
Aeriicatmeow <neranweaver@gmail.com>
2026-08-21 19:54:36 +0100

    incremented version number

--- NuclearOptionVWS/Plugin.cs
+++ NuclearOptionVWS/Plugin.cs
@@ -22,7 +22,7 @@ using NuclearOption.Debugging;
 
 namespace NuclearOptionVWS;
 
-[BepInPlugin("com.Aeriicatmeow.NuclearOptionVWS", "NuclearOption-VWS", "1.2.3")]
+[BepInPlugin("com.Aeriicatmeow.NuclearOptionVWS", "NuclearOption-VWS", "1.2.4")]
 public class Plugin : BaseUnityPlugin
 {
 
